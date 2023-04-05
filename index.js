d84042132541e6ba7de524e710226150ae70f678
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-04-05 18:09:14 +0300

    Prevent my second form's default action

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const searchForm = document.getElementById('mysearch')
+const subscriptionForm = document.getElementById('subscription-form')
+
+subscriptionForm.addEventListener('submit', (e) => e.preventDefault())
 searchForm.addEventListener('submit', (e) => e.preventDefault())
 
 const main = document.querySelector('main')
@@ -7,16 +10,19 @@ const main = document.querySelector('main')
 fetch('https://thronesapi.com/api/v2/Characters')
     .then(res => res.json())
     .then(character=> {
-        let card = document.createElement('div');
-        card.innerHTML = `
+        let myCharacter = document.createElement('div');
+        myCharacter.id = "my-character";
+        myCharacter.innerHTML = `
             <img src="${character[0].imageUrl}" alt="daenerys.jpg">
+            <div id = "character-details">
             <p>Family : ${character[0].family}</p>
             <p>FirstName: ${character[0].firstName}</p>
             <p>Fullname: ${character[0].fullName}</p>
             <p>LastName: ${character[0].lastName}</p>
             <p>Title: ${character[0].title}</p>
+            </div>
             `
-            main.appendChild(card)
+            main.appendChild(myCharacter)
     })
 
     

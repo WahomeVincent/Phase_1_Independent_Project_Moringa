80fd08b2b24c56ec49b10f110171c28526f56eab
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-04-05 14:14:56 +0300

    Display only one character

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const searchForm = document.getElementById('mysearch')
 searchForm.addEventListener('submit', (e) => e.preventDefault())
 
+const main = document.querySelector('main')
+
 
 fetch('https://thronesapi.com/api/v2/Characters')
     .then(res => res.json())
-    .then(characters => {
-       
+    .then(character=> {
+        let card = document.createElement('div');
+        card.innerHTML = `
+            <img src="${character[0].imageUrl}" alt="daenerys.jpg">
+            <p>Family : ${character[0].family}</p>
+            <p>FirstName: ${character[0].firstName}</p>
+            <p>Fullname: ${character[0].fullName}</p>
+            <p>LastName: ${character[0].lastName}</p>
+            <p>Title: ${character[0].title}</p>
+            `
+            main.appendChild(card)
     })
 
-    // const main = document.querySelector('main')
-    //         let card = document.createElement('div')
-    //         card.innerHTML = `
-    //         <p>Family : ${character.family}</p>
-    //         <p>FirstName: ${character.firstName}</p>
-    //         <p>Fullname: ${character.fullName}</p>
-    //         <img src="${character.imageUrl}" alt="daenerys.jpg">
-    //         <p>LastName: ${character.lastName}</p>
-    //         <p>Title: ${character.title}</p>
-    //         `
-    //         main.appendChild(card)
+    

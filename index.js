9f6f284513dffa418096f5af552ade3be130231f
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-04-05 13:51:51 +0300

    Prevent the default search-form submission

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+const searchForm = document.getElementById('mysearch')
+searchForm.addEventListener('submit', (e) => e.preventDefault())
+
+
 fetch('https://thronesapi.com/api/v2/Characters')
     .then(res => res.json())
     .then(characters => {
-        characters.forEach(character => {
-            const main = document.querySelector('main')
-            let card = document.createElement('div')
-            card.innerHTML = `
-            <p>Family : ${character.family}</p>
-            <p>FirstName: ${character.firstName}</p>
-            <p>Fullname: ${character.fullName}</p>
-            <img src="${character.imageUrl}" alt="daenerys.jpg">
-            <p>LastName: ${character.lastName}</p>
-            <p>Title: ${character.title}</p>
-            `
-            main.appendChild(card)
-        });
+       
     })
+
+    // const main = document.querySelector('main')
+    //         let card = document.createElement('div')
+    //         card.innerHTML = `
+    //         <p>Family : ${character.family}</p>
+    //         <p>FirstName: ${character.firstName}</p>
+    //         <p>Fullname: ${character.fullName}</p>
+    //         <img src="${character.imageUrl}" alt="daenerys.jpg">
+    //         <p>LastName: ${character.lastName}</p>
+    //         <p>Title: ${character.title}</p>
+    //         `
+    //         main.appendChild(card)

5d6372466847b7526d0927a314d1b46eace5b702
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-04-05 13:26:21 +0300

    Display each character details on the DOM

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-fetch('https://thronesapi.com/api/v1/Characters')
+fetch('https://thronesapi.com/api/v2/Characters')
     .then(res => res.json())
-    .then(characters => console.log(characters))
+    .then(characters => {
+        characters.forEach(character => {
+            const main = document.querySelector('main')
+            let card = document.createElement('div')
+            card.innerHTML = `
+            <p>Family : ${character.family}</p>
+            <p>FirstName: ${character.firstName}</p>
+            <p>Fullname: ${character.fullName}</p>
+            <img src="${character.imageUrl}" alt="daenerys.jpg">
+            <p>LastName: ${character.lastName}</p>
+            <p>Title: ${character.title}</p>
+            `
+            main.appendChild(card)
+        });
+    })
